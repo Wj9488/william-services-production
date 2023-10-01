9fba425fadf758536d99e3ee2c7b6c42b5ab7ea0
Will Jones <wtjones10@gmail.com>
2023-10-01 23:45:31 +0100

    font fix attempt 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import localFont from "next/font/local"
+
+const degularFont = localFont({ src: "./Fonts/Degular-Regular.woff"}) 
 
 export const metadata: Metadata = {
   title: 'william.services',
@@ -13,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="overflow-x-hidden">{children}</body>
+      <body className={`overflow-x-hidden ${degularFont.className}`}>{children}</body>
     </html>
   )
 }
